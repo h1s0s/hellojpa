58f27bf523c02d3482e73acaf4621ce9e502ec9f
h1s0s <hs94122@naver.com>
2023-03-21 15:54:17 +0900

    java ORM generation JPA programing - 기본문법과 쿼리 API

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * packageName  : hellojpa
@@ -27,6 +25,36 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //JPQL 문법
+
+            //////////
+            //쿼리 요청
+            //////////
+            //대소문자는 java 필드, 클래스에 맞게 사용해야 함
+            //나머지 문법은 대소문자 구분하지 않음
+            //테이블명이 아닌 엔티티 이름사용, 별칭 필수
+
+            //TypedQuery : 반환타입이 명확할 때 사용
+            //Query: 반환타입이 명확하지 않을 때 사용
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            List<Member> query1List = query1.getResultList();//리스트로 반환 받을때 : 결과가 없으면 빈 리스트를 반환(NullPointException 걱정 X)
+            Member query1One = query1.getSingleResult();//객체 1개로 반환 받을때 : 결과가 정확히 하나여야함 결과가 없으면 NoResultException, 둘 이상히면 NonUniqueResultException
+
+            Query query2 = em.createQuery("select m.username, m.age from Member m");
+
+            ///////////////
+            //파라미터 바인딩
+            ///////////////
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                                    .setParameter("username", "member1")//":키값" 에 값 바인딩
+                                    .getSingleResult();
+            System.out.println("singleResult = " + singleResult);
+
 
             tx.commit();
         } catch (Exception e){
